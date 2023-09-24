f988c06fabad7b2f4840c3e37e0abc9270763bb3
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-09-24 08:34:10 +0530

    Handling form error using ResultBinding interface and Spring MVC form tag liberary

--- src/main/java/springmvc/controller/ContactController.java
+++ src/main/java/springmvc/controller/ContactController.java
@@ -1,18 +1,22 @@
 package springmvc.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 import springmvc.model.User;
 
 @Controller
-public class ContactController {
-	@RequestMapping(path = "/sendmessage", method = RequestMethod.POST)
-	public String sendMessage(@ModelAttribute User user, Model model) {
+public class ContactController {	
+	
+	@RequestMapping(path="/sendmessage", method= RequestMethod.POST)
+	public String sendMessage(@ModelAttribute("user") User user,BindingResult result,Model model) {
+		if(result.hasErrors()) {
+			System.out.println(result.getErrorCount());
+			System.out.println(result.getFieldValue("id"));
+			System.out.println(result.getAllErrors());
+			return "contact";
+		}
 		return "about";
 	}
-
 }
